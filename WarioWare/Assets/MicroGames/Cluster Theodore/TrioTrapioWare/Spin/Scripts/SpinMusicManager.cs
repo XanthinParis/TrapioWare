cdcecd0e53b20adcccbaddc9da746d21230de7fe
Antoine <grugeon.antoine@gmail.com>
2021-01-15 15:58:34 +0100

    fix 2

---
+++ WarioWare/Assets/MicroGames/Cluster Theodore/TrioTrapioWare/Spin/Scripts/SpinMusicManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Testing;
+
+namespace TrapioWare
+{
+    namespace Spin
+    {
+        public class SpinMusicManager : TimedBehaviour
+        {
+            public BpmMusic[] musics;
+
+            private AudioSource source;
+
+            public override void Start()
+            {
+                base.Start();
+                source = GetComponent<AudioSource>();
+                AudioClip currentMusic = null;
+                for(int i = 0; i < musics.Length; i++)
+                {
+                    if(musics[i].bpm == bpm)
+                    {
+                        currentMusic = musics[i].music;
+                    }
+                }
+
+                if(currentMusic != null)
+                {
+                    source.clip = currentMusic;
+                    source.Play();
+                }
+            }
+        }
+
+        [System.Serializable]
+        public class BpmMusic
+        {
+            public AudioClip music;
+            public float bpm;
+        }
+    }
+}
